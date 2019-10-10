ccd8d588bfd725327f9c39b40b2a5de73f6d5d90
guerraj <guerraj@usservicesinc.com>
2019-10-10 11:02:11 -0400

    removed properties

--- AnotherStorageImplementation/Properties/AssemblyInfo.cs
+++
@@ -1,7 +0,0 @@
-﻿using System.Reflection;
-using System.Runtime.CompilerServices;
-using System.Runtime.InteropServices;
-
-[assembly: ComVisible(false)]
-
-[assembly: Guid("ff6b5fbc-8a64-42d7-b52d-57d788e5a518")]
